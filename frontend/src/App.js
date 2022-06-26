929a3317b7a0a6db091f2e68f0b320d1f5a05505
Rafa <BIT18-207@pugc.edu.pk>
2022-06-26 23:39:23 +0500

    React setup2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 const App = () => {
   return (
     <>
-      <h1>Proshop</h1>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <HomeScreen />
+        </main>
+      </Container>
+      <Footer />
     </>
   );
 };
