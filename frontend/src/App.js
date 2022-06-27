fdd0040c9176d6ef1ec3dfe7d4e62619d35a8d2c
Rafa <BIT18-207@pugc.edu.pk>
2022-06-27 22:47:23 +0500

    React setup 4 Frontend Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,31 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <Container>
-        <main className="py-3">
-          <HomeScreen />
-        </main>
-      </Container>
+
+      <main className="py-3">
+        <Container>
+          <Routes>
+            {" "}
+            <Route exact path="/" element={<HomeScreen />}></Route>
+            <Route
+              exact
+              path="/product/:id"
+              element={<ProductScreen />}
+            ></Route>
+          </Routes>
+        </Container>
+      </main>
+
       <Footer />
-    </>
+    </Router>
   );
 };
 
